328859f7816f04f66517958f78da4538251da33f
13750465 <ian941219@gmail.com>
2025-10-08 11:41:23 +0800

    week05

---
+++ week05/week05-1.cpp
@@ -0,0 +1,17 @@
+// week05-1.cpp
+// Leetcode ¾Ç²ß­pµe Built-In 58. Length of Last Word
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        stringstream ss(s);
+        string word;
+        //ss >> word;
+        //ss >> word;
+        while( ss >> word){
+
+        }
+        cout << word;
+
+        return word.length();
+    }
+};
